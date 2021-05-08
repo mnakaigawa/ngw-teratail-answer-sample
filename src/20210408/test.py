001b289f628cfaeb974531ff9677fbda38f9fb25
NGW <tnkngw29@gmail.com>
2021-05-09 07:45:11 +0900

    #7 Corrected and refactored the points made by otn.

--- src/20210408/test.py
+++ src/20210408/test.py
@@ -1,6 +1,13 @@
-    for max_num_comment in driver.find_element_by_xpath("//tbody/tr/td"):
-        max_num = max_num_comment.text
-        max_num = max_num.replace(" ", "")
-        print("----------------------")
-        print(max_num_comment)#text
-        print("----------------------")
+#!/usr/bin/env python
+# -*- coding:utf-8-*-
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+driver.get('sample.html')
+
+for max_num_comment in driver.find_elements_by_xpath("//tbody/tr/td"):
+    max_num = max_num_comment.text
+    max_num = max_num.replace(" ", "")
+    print("----------------------")
+    print(max_num_comment)#text
+    print("----------------------")
