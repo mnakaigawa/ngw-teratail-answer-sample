f4c3ddad7b8961ffad4dfb8962bd13b2fbb6143c
NGW <tnkngw29@gmail.com>
2021-05-09 07:41:28 +0900

    Update answer of 20210323.

--- src/20210323/README.md
+++ src/20210323/README.md
@@ -2,4 +2,5 @@
 https://teratail.com/questions/329307
 
 ## issue
+[#1](https://github.com/mnakaigawa/ngw-teratail-answer-sample/issues/1)
 
--- src/20210323/mysql.py
+++ src/20210323/mysql.py
@@ -1,28 +1,25 @@
 #!/usr/bin/env python
 # -*- coding:utf-8-*-
-'''
-Created on 2018/04/13
+import pymysql
+# MySQLに接続する
+connection = pymysql.connect(host='localhost',
+                            user='ユーザー名',
+                            password='パスワード',
+                            db='データベース名',
+                            charset='utf8',
+                            cursorclass=pymysql.cursors.DictCursor)
+# SQLを操作する
+with connection.cursor() as cursor:
+    #「my_table」から「tw_id」が重複を省いた仮テーブル「my_table_temp」を作成する
+    sql = "Select * from START_END_TIME where Start_datetime >= 2021/3/1 00:00:00 order by Start_datetime;"
+    cursor.execute(sql)
+    records = cursor.fetchall()
 
-@author: nakaigawa
-'''
-import MySQLdb
+    new_records = [[records[idx, 1], records[idx+1, 1], records[2]] for idx in range(len(records))]
 
-conn = MySQLdb.connect(
- user='root',
- passwd='root',
- host='localhost',
- db='mysql')
+# MySQLから切断する
+connection.close()
 
-cur = conn.cursor()
-sql = "select * from START_END_ITEM order by Start_datetime;"
-cur.execute(sql)
-
-# 実行結果を取得する
-rows = cur.fetchall()
-
-# 一行ずつ表示する
-for row in rows:
- print(row)
-
-cur.close
-conn.close
+# 結果表示
+for record in new_records:
+    print(record)
